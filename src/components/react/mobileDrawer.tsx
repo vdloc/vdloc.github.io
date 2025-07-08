522a4841b2e211bc5d137706dcef547d843b2b0a
vdloc <uvcudlco@gmail.com>
2025-07-08 23:11:20 +0700

    feat: restructure PageLayout to improve layout styling and maintain responsiveness

--- src/components/react/mobileDrawer.tsx
+++ src/components/react/mobileDrawer.tsx
@@ -7,26 +7,33 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Button } from "@/components/ui/button";
-import React from "react";
+} from '@/components/ui/drawer';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+import { EyeIcon } from 'lucide-react';
 
 export default function MobileDrawer() {
   return (
-    <Drawer>
-      <DrawerTrigger>Open</DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-          <DrawerDescription>This action cannot be undone.</DrawerDescription>
-        </DrawerHeader>
-        <DrawerFooter>
-          <Button>Submit</Button>
-          <DrawerClose>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+    <aside className='md:hidden'>
+      <Drawer>
+        <DrawerTrigger className='cursor-pointer'>
+          <EyeIcon className='h-6 w-6' />
+        </DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle className='text-4xl'>
+              Are you absolutely sure?
+            </DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant='outline'>Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </aside>
   );
 }
